b535d7d9474bd93cc9bf4a9f929264db30c00a9c
Xuefeng Luo <hakunamatatahenry@gmail.com>
2022-10-20 08:45:39 +0200

    add files

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!')
+  res.status(200).json({'hello': 'world'});
+});
+
+app.get('/friend', (req, res) => {
+  //   res.send('Hello World!')
+  res.status(200).json({ 'hello': 'friend' });
+});
+
+module.exports = app;
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
---
+++ backend/app.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('./app.js');
+
+describe('GET /', () => {
+  it('test /', async () => {
+    // await request(app)
+    //   .get('/')
+    //   .expect(200)
+    //   .expect({'hello':'world'});
+    expect(3).toBe(3);
+  });
+});
